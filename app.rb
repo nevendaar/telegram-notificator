53faf343c2d99e18d6e5f3fde8e64ba800beaa97
Ivan <vansan@nevendaar.com>
2017-05-20 17:41:09 +0300

    Basic auth support

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
+# frozen_string_literal: true
+
 require 'sinatra'
+
+def secure_digest(str)
+  Digest::SHA512.digest str
+end
+private :secure_digest
+
+BASIC_AUTH_LOGIN    = secure_digest(ENV.fetch('BASIC_AUTH_LOGIN')).freeze
+BASIC_AUTH_PASSWORD = secure_digest(ENV.fetch('BASIC_AUTH_PASSWORD')).freeze
+
+use Rack::Auth::Basic, 'Restricted Area' do |login, password|
+  Rack::Utils.secure_compare(secure_digest(login), BASIC_AUTH_LOGIN) &&
+    Rack::Utils.secure_compare(secure_digest(password), BASIC_AUTH_PASSWORD)
+end
 
 get '/' do
   'Form will be here'
